13d4f64280486f3f7ab805c8681a97836be882b3
bektechie <https://github.com/bektechie>
2025-09-23 19:20:57 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+
+console.log("web Serverni Boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
+
+//1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2 Sesion code
+
+//3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"})
+})
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+app.get("/", function(req, res) {
+    res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The sever is running successfully on port: ${PORT}`);
+});
---
+++ train.js
@@ -0,0 +1,167 @@
+// // train.js
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//     "yahshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60+
+// ];
+
+// function maslahatBering(a, callback) {
+//     if (typeof a !== "number") {
+//         callback("insert a number", null);
+//     } else if (a <= 20) {
+//         callback(null, list[0]);
+//     } else if (a > 20 && a <= 30) {
+//         callback(null, list[1]);
+//     } else if (a > 30 && a <= 40) {
+//         callback(null, list[2]);
+//     } else if (a > 40 && a <= 50) {
+//         callback(null, list[3]);
+//     } else if (a > 50 && a <= 60) {
+//         callback(null, list[4]);
+//     } else {
+//         callback(null, list[5]);
+//     }
+// }
+
+// maslahatBering(65, (err, data) => {
+//     if (err) {
+//         console.log('ERROR:', err);
+//         return; // stop execution if there’s an error
+//     }
+//     console.log('javob:', data);
+// });
+// train.js
+
+
+
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//     "yahshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60+
+// ];
+
+// function maslahatBering(a, callback) {
+//     if (typeof a !== "number") {
+//         callback("insert a number", null);
+//     } else if (a <= 20) {
+//         callback(null, list[0]);
+//     } else if (a > 20 && a <= 30) {
+//         callback(null, list[1]);
+//     } else if (a > 30 && a <= 40) {
+//         callback(null, list[2]);
+//     } else if (a > 40 && a <= 50) {
+//         callback(null, list[3]);
+//     } else if (a > 50 && a <= 60) {
+//         callback(null, list[4]);
+//     } else {
+//         callback(null, list[5]);
+//     }
+// }
+
+// maslahatBering(46, (err, data) => {
+//     if (err) {
+//         console.log('ERROR:', err);
+//         return; // stop execution if there’s an error
+//     }
+//     console.log('javob:', data);
+// });
+
+
+
+// MIT TASK //
+// function countLetter(letter, str) {
+//     let count = 0;
+//     for (let c of str) if (c === letter) count++;
+//     return count;
+// }
+
+// // Example:
+// console.log(countLetter("a", "Abdulazizbek")); // 3
+
+
+// console.log("Jack Ma maslahatlari");
+
+const list = [
+    "yahshi talaba boling", // 0-20
+    "togri boshliq tanlang va koproq hato qiling", // 20-30
+    "uzingizga ishlashingizni boshlang", // 30-40
+    "siz kuchli bolgan narsalarni qiling", // 40-50
+    "yoshlarga investitsiya qiling", // 50-60
+    "endi dam oling, foydasi yoq endi", // 60+
+];
+
+// async function maslahatBering(a) {
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a < 20) return list[0];
+//     else if (a >= 20 && a < 30) return list[1];
+//     else if (a >= 30 && a < 40) return list[2];
+//     else if (a >= 40 && a < 50) return list[3];
+//     else if (a >= 50 && a < 60) return list[4];
+//     else {
+//       return new Promise((resolve,reject) => {
+//         setTimeout(()=> {
+//             resolveInclude(list(5))
+//         },5000);
+//       });
+//     }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(25)
+//     .then((data) => {
+//         console.log("javob:", data);
+//     })
+//     .catch((err) => {
+//         console.log("ERROR:", err);
+//     });
+// console.log("passed here 1");
+
+// async function run() {
+// let javob = await maslahatBering(20);
+// console.log(javob);
+// javob = await maslahatBering(31);
+// console.log(javob);
+// javob = await maslahatBering(41);
+// console.log(javob);
+// }
+// run(); 
+
+    // async function run() {
+    //     let javob = await maslahatBering(20);
+    //     console.log(javob);
+    // }
+    // run();
+
+
+function maslahatBering(a, callback) {
+    if (typeof a !== "number") callback("Insert a number, please");
+    else if (a < 20) callback(null, list[0]);
+    else if (a < 30) callback(null, list[1]);
+    else if (a < 40) callback(null, list[2]);
+    else if (a < 50) callback(null, list[3]);
+    else if (a < 60) callback(null, list[4]);
+    else {
+        setInterval(function () {
+            callback(null, list[5]);
+        }, 1000);
+    }
+}
+
+console.log("passed here 0");
+maslahatBering(70, (err, data) => {
+    if (err) console.log("ERROR", err);
+    else {
+        console.log(data);
+    }
+});
+console.log("passed here 1");
